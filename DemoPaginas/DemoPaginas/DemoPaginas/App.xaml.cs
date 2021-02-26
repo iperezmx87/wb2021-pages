9fc3fd502092db3127847f6dff14e0cbf0057329
ISRAEL PEREZ SAUCEDO <isra.saucedo50@outlook.com>
2021-02-25 20:25:26 -0600

    Carga inicial

---
+++ DemoPaginas/DemoPaginas/DemoPaginas/App.xaml.cs
@@ -0,0 +1,61 @@
+﻿using DemoPaginas.Vistas;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DemoPaginas
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            // por default
+            //MainPage = new MainPage();
+
+            // vista de tabs
+            VistaTabbed vistaTabs = new VistaTabbed();
+
+            // agregar contenido a las tabs
+            MainPage mPage = new MainPage();
+            mPage.Title = "Contenido";
+
+            // agregando paginas de contenido a la tab
+            vistaTabs.Children.Add(mPage);
+
+            VistaFlyout vistaMaster = new VistaFlyout { 
+                Detail = vistaTabs
+            };
+
+            // colocando la vista master detail como la principal del app
+            MainPage = vistaMaster;
+
+            //tbpInicial newTab = new tbpInicial();
+
+            //MainPage mPage = new MainPage();
+            //mPage.Title = "Inicial";
+
+            //newTab.Children.Add(mPage);
+            //newTab.Children.Add(new ContentPage
+            //{
+            //    Title = "tab2"
+            //});
+
+            //MainPage = newTab;
+
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ DemoPaginas/DemoPaginas/DemoPaginas/Vistas/Menu.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DemoPaginas.Vistas
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Menu : ContentPage
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            // acceder a la vista principal del app
+            ((VistaFlyout)App.Current.MainPage).Detail = new VistaTabbed();
+            ((VistaFlyout)App.Current.MainPage).IsPresented = false;
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            ((VistaFlyout)App.Current.MainPage).Detail = new NavigationPage(new ContentPage());
+            ((VistaFlyout)App.Current.MainPage).IsPresented = false;
+        }
+    }
+}
